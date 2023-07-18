d70448603935e918c7c9338166629a25a46abba2
codejunkie312 <anas.sahimi1@gmail.com>
2023-07-17 19:27:39 -0700

    0x0E Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * Description: A type struct called dog with name, age and owner elements
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
