fe6b5e53e3b9bb27a898f74eec26d616195bbe13
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-08-04 01:41:32 +0100

    0x14 Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get from
+ * one number to another
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, count = 0;
+
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if ((n & 1) != (m & 1))
+			count++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return (count);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -8,3 +8,5 @@
 * sets the value of a bit to `1` at a given index.
 # `int clear_bit(unsigned long int *n, unsigned int index);`
 * sets the value of a bit to 0 at a given index.
+# `unsigned int flip_bits(unsigned long int n, unsigned long int m);`
+* returns the number of bits you would need to flip to get from one number to another.
