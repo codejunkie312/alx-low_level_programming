6e85d7946a849bcd2855170c1a04cd6c501449df
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-08-04 01:23:03 +0100

    0x14 Task 0

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+# `unsigned int binary_to_uint(const char *b);`
+* converts a binary number to an `unsigned int`.
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Prototypes */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+
+#endif /* MAIN_H */
