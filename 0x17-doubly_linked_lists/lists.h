68fb7a1ff35c73b61160e498660cd468169a805b
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-09-15 00:41:07 +0100

    0x17 Task 0

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,32 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * Description: singly linked list node structure for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} dlistint_t;
+
+/* Prototypes */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int sum_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
+
+#endif /* LISTS_H */
